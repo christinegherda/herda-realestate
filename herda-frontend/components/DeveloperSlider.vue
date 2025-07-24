3b4d8cfbb25a5f5c390f0da6403a9e7df8fe2c1d
christinesds <christine@esds.com.ph>
2025-07-24 14:12:10 +0800

    udpate

--- herda-frontend/components/DeveloperSlider.vue
+++ herda-frontend/components/DeveloperSlider.vue
@@ -5,30 +5,32 @@
       <pre>
         {{ developers }}
       </pre>
-      <swiper
-        effect="fade" 
-        :slides-per-view="6"
-        :space-between="10"
-        :breakpoints="{
-          768: { slidesPerView: 3 },
-          1024: { slidesPerView: 6 }
-        }"
-        :loop="true"
-        :autoplay="{ delay: 100 }"
-        :pagination="true"
-        class="w-full"
-      >
-        <swiper-slide v-for="developer in developers" :key="developer.id" class="flex justify-center items-center">
-          <div class="bg-white p-2 w-full max-w-[200px] h-[100px] flex justify-center items-center">
-            <img
-              v-if="developer.logo"
-              :src="`${config.public.images}/${developer.logo}`"
-              :alt="developer.name"
-              class="max-h-16 object-contain"
-            />
-          </div>
-        </swiper-slide>
-      </swiper>
+      <div v-if="developers">
+        <swiper
+          effect="fade" 
+          :slides-per-view="6"
+          :space-between="10"
+          :breakpoints="{
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 6 }
+          }"
+          :loop="true"
+          :autoplay="{ delay: 100 }"
+          :pagination="true"
+          class="w-full"
+        >
+          <swiper-slide v-for="developer in developers" :key="developer.id" class="flex justify-center items-center">
+            <div class="bg-white p-2 w-full max-w-[200px] h-[100px] flex justify-center items-center">
+              <img
+                v-if="developer.logo"
+                :src="`${config.public.images}/${developer.logo}`"
+                :alt="developer.name"
+                class="max-h-16 object-contain"
+              />
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
     </div>
   </div>
 </template>
@@ -44,13 +46,21 @@ import 'swiper/css/effect-fade'
 import 'swiper/css/autoplay'
 
 const config = useRuntimeConfig();
-
-const { data: developers } = await useAsyncData('developers', () =>
+ 
+const developers = ref([])
+const  { data: res }  = await await useAsyncData('developers', () =>
   $fetch(`${useRuntimeConfig().public.apiBase}/developers`),
   { lazy: true }
 )
+console.log('Client fetch:', res)
+developers.value = res.value
 
-console.log(developers);
+watchEffect(() => {
+  console.log('Fetched developers:', developers.value)
+})
+
 console.log(developers.value);
-console.log(`${config.public.apiBase}/developers`);
+console.log(`${config.public.apiBase}/developers`); 
+
+
 </script>
